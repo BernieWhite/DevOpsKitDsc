74f95a23927aa0a74e2d5693ba6d3218d971f4be
Bernie White <bewhite@microsoft.com>
2017-11-22 19:46:19 +1000

    Updates to documentation build script

--- scripts/build-docs.ps1
+++ scripts/build-docs.ps1
@@ -1,13 +1,18 @@
 
 [CmdletBinding()]
 param (
-    [Switch]$Scaffold = $False`
+    [Switch]$Scaffold = $False
 )
 
-Import-Module '.\src\DevOpsKitDsc' -Force;
-
-
 if ($Scaffold) {
+    Import-Module '.\src\DevOpsKitDsc' -Force;
 
     Update-MarkdownHelp -Path '.\docs\commands\en-US';
+
+    return;
 }
+
+New-ExternalHelp -OutputPath '.\build\docs' -Path '.\docs\commands\en-US' -Force;
+
+Copy-Item -Path '.\build\docs\DevOpsKitDsc-help.xml' -Destination '.\src\en-US';
+Copy-Item -Path '.\build\docs\DevOpsKitDsc-help.xml' -Destination '.\src\en-AU';
