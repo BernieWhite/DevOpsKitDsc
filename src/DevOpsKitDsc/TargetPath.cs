23f1438cfdc927b86d2a569cc57c4a5b6d11f2b2
Bernie White <bewhite@microsoft.com>
2018-04-12 23:24:16 +1000

    Added schema support for publishing for Azure DSC extensions #3

---
+++ src/DevOpsKitDsc/TargetPath.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace DevOpsKitDsc
+{
+    public sealed class TargetPath
+    {
+        [JsonProperty(PropertyName = "path", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Path { get; set; }
+
+        [JsonProperty(PropertyName = "sasToken", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string SasToken { get; set; }
+
+        public static implicit operator TargetPath(string value)
+        {
+            return new TargetPath {
+                Path = value
+            };
+        }
+    }
+}
